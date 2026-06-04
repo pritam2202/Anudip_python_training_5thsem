bf6b2f868cd510fc7494977167634241197b1268
pritam2202 <pritamkumar854311@gmail.com>
2026-06-04 11:06:37 +0530

    Create battery charging.py

---
+++ CLASSWORK/4TH JUNE/battery charging.py
@@ -0,0 +1,8 @@
+charging level =20 
+ while(charging_level=100);
+     if(charging_level):
+      print("battery level is",charging_level,"%")
+      charging_level=charging_level+10
+    else:
+         break
+  print("full charge")
