afa72bdad344f29137d61bc4c6395fe842622ddc
pritam2202 <pritamkumar854311@gmail.com>
2026-06-05 11:20:15 +0530

    Create triangleside.py

---
+++ self_learning/triangleside.py
@@ -0,0 +1,16 @@
+#check the three side from the traingle or not
+#check the three side from the traingle or not
+a = float(input("Enter the length of first side : "))
+b = float(input("Enter the length of second side : "))
+c = float(input("Enter the length of third side : "))
+#-------------------check---------------------------------
+if (a+b>c) and (b+c>a) and (c+a>b):
+    print("the three sides of the traingle")
+else:
+    print("the three sides are not of the traingle")
+#-------------------check---------------------------------
+if (a+b>c) and (b+c>a) and (c+a>b):
+    print("the three sides of the traingle")
+else:
+    print("the three sides are not of the traingle")
+
