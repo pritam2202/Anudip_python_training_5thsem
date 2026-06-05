704b353d0bebb95853d4ccf31316f3a6e634180c
pritam2202 <pritamkumar854311@gmail.com>
2026-06-05 13:19:36 +0530

    Update Armstrong Number Checker .py

--- LABWORK/4TH JUNE/Armstrong Number Checker .py
+++ LABWORK/4TH JUNE/Armstrong Number Checker .py
@@ -1,15 +1,18 @@
+# program to check if the number is armstrong or not
+#---------------------------------------------------------------
+print("---------------Armstrong Number---------------")
+# take the number form user
 num = int(input("Enter a number: "))
-
+sum = 0
 temp = num
-digits = len(str(num))
-armstrong_sum = 0
-
+# checking the number is armstrong if the sum of the cube of the digit is equal to the number
 while temp > 0:
     digit = temp % 10
-    armstrong_sum += digit ** digits
-    temp //= 10
-
-if armstrong_sum == num:
+    sum = sum + digit ** 3
+    temp = temp // 10
+# the number is armstrong
+if sum == num:
     print("Armstrong Number")
+# the number is not an armstrong    
 else:
     print("Not an Armstrong Number")
