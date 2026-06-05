3d089b8fc759000191ff400e7c88f6c9f78cc217
pritam2202 <pritamkumar854311@gmail.com>
2026-06-05 10:07:08 +0530

    Create  Electricity Bill Calculator .py

---
+++ LABWORK/4TH JUNE/Electricity Bill Calculator .py
@@ -0,0 +1,19 @@
+units = int(input("Enter units consumed: "))
+
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+else:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+
+if units <= 100:
+    category = "Low Consumption"
+elif units <= 200:
+    category = "Medium Consumption"
+else:
+    category = "High Consumption"
+
+print("Units Consumed:", units)
+print("Total Bill: ₹", bill)
+print("Category:", category)
