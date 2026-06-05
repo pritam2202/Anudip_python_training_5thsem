578848477682f0232d7c4d66735442ab13763745
pritam2202 <pritamkumar854311@gmail.com>
2026-06-05 13:13:36 +0530

    Update battery charging.py

--- CLASSWORK/4TH JUNE/battery charging.py
+++ CLASSWORK/4TH JUNE/battery charging.py
@@ -1,8 +1,12 @@
-charging level =20 
- while(charging_level=100);
-     if(charging_level):
-      print("battery level is",charging_level,"%")
-      charging_level=charging_level+10
+#program for displaying battery charging level
+charging_level = 20
+electricity_status = True
+while(charging_level <= 100):
+    if(electricity_status):
+      print("battery level : ",charging_level,"%")
+      charging_level = charging_level + 10
     else:
-         break
-  print("full charge")
+       break
+ #------------------------------------------------------------
+print("Full charge")    
+
